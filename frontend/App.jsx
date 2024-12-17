f7e4e5192f2809e0ac22fa8cda0245ffbf1e54c7
apinkhasov101 <apinkhasov101@gmail.com>
2024-12-17 10:19:13 -0600

    Arnold and Sienna - React & Redux

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,9 +1,41 @@
 import React from "react"
+import { createSlice } from '@reduxjs/toolkit'  // imports react components?
 
 const App = () => {
     return (
-        <h1>hi</h1>
+        <main>
+            <h1>Yeti Crab Mortgage Calculator</h1>
+        <div>
+            <input type='number' name='principle' placeholder='Principle Amt'></input>
+        </div>
+        <br></br>
+        <div>            
+            <select
+                name='loanTerm'
+                value=''
+                onChange=''
+            >
+            <option value=''>Select Loan Term</option>
+            <option value='15'>15 Years</option>
+            <option value='20'>20 Years</option>
+            <option value='30'>30 Years</option>
+            </select>
+        </div>
+        <br></br>
+        <div>
+            <input type='number' name='loanAmt' placeholder='Loan Amt'></input>
+        </div>
+        <br></br>
+        <div>
+            <input type='number' name='homeInsurance' placeholder='Home Insurance'></input>
+        </div>
+        </main>
     )
 }
 
+// ! 'npm install @reduxjs/toolkit react-redux' installs redux toolkit
+
 export default App
+
+
+    
