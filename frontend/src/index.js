79c848aa892ae0dd419a7aded18f8c053efcb40f
Jayson Sanon <jaysonsanon@Jaysons-MacBook-Air.local>
2024-12-17 01:13:44 -0500

    webpack finished

---
+++ frontend/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+// import App from './components/App.jsx';
+import styles from '../../styles.css'
+
+let root = createRoot(document.getElementById('root'))
+
+
+root.render(
+    //  <App />
+    <p>test</p>
+);
+
